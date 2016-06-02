e8bc8a0c6ec0b0bf5d82316861262e0c4ce8479d
Maarten Paauw <maartenpaauw@gmail.com>
2016-06-02 14:47:21 +0200

    Klasse voor led aangemaakt

---
+++ Led.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+
+class Led:
+
+    def __init__(self, output_pin: int) -> None:
+
+        self._led_output_pin = output_pin
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self._led_output_pin, GPIO.OUT)
+
+    def blink(self, seconds: int):
+        GPIO.output(self._led_output_pin, GPIO.HIGH)
+        time.sleep(seconds)
+        GPIO.output(self._led_output_pin, GPIO.LOW)
+        time.sleep(seconds)
+
+def main() -> None:
+    led = Led(12)  # input pin
+    led.blink(2)
+
+if __name__ == '__main__':
+    main()
