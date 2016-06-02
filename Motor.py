e66cef094d8acfd906ab1da3006ad11e3fd89a45
Maarten Paauw <maartenpaauw@gmail.com>
2016-06-02 12:37:40 +0200

    Up en Down methode toegevoegd aan de Motor klasse

--- Motor.py
+++ Motor.py
@@ -1,17 +1,48 @@
 import RPi.GPIO as GPIO
+import time
 
 
 class Motor:
 
+    CONST_SEQUENCE = ['1000', '0100', '0010', '0001']
+
     def __init__(self, input_pins: list) -> None:
-        GPIO.setup(input_pins[0], GPIO.OUT)
-        GPIO.setup(input_pins[1], GPIO.OUT)
-        GPIO.setup(input_pins[2], GPIO.OUT)
-        GPIO.setup(input_pins[3], GPIO.OUT)
+
+        GPIO.setmode(GPIO.BCM)
+
+        self._coil_A_1_pin = input_pins[0]
+        self._coil_A_2_pin = input_pins[1]
+        self._coil_B_1_pin = input_pins[2]
+        self._coil_B_2_pin = input_pins[3]
+
+        GPIO.setup(self._coil_A_1_pin, GPIO.OUT)
+        GPIO.setup(self._coil_A_2_pin, GPIO.OUT)
+        GPIO.setup(self._coil_B_1_pin, GPIO.OUT)
+        GPIO.setup(self._coil_B_2_pin, GPIO.OUT)
+
+    def set_step(self, step: str) -> bool:
+        GPIO.output(self._coil_A_1_pin, int(step[0]) == 1)
+        GPIO.output(self._coil_A_2_pin, int(step[1]) == 1)
+        GPIO.output(self._coil_B_1_pin, int(step[2]) == 1)
+        GPIO.output(self._coil_B_2_pin, int(step[3]) == 1)
+
+    def up(self, steps: int) -> bool:
+        for i in range(steps):
+            for step in list(reversed(self.CONST_SEQUENCE)):
+                self.set_step(step)
+                time.sleep(0.005)
+
+    def down(self, steps: int) -> bool:
+        for i in range(steps):
+            for step in self.CONST_SEQUENCE:
+                self.set_step(step)
+                time.sleep(0.005)
 
 
 def main() -> None:
-    Motor([17, 18, 27, 22])
+    motor = Motor([26, 13, 6, 5])
+    motor.up(1)
+    motor.down(1)
 
 if __name__ == '__main__':
     main()
