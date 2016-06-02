4bcb2fa91020cddc456526c905f38626a4c4eb4f
Owain van Brakel <owain.vanbrakel@gmail.com>
2016-06-02 18:51:31 +0200

    Tabs to spaces

--- rik.py
+++ rik.py
@@ -20,58 +20,58 @@ GPIO.setup(coil_B_2_pin, GPIO.OUT)
 sequence = ['1000', '0100', '0010', '0001']
 
 def set_step(step):
-		GPIO.output(coil_A_1_pin, step[0] == '1')
-		GPIO.output(coil_A_2_pin, step[1] == '1')
-		GPIO.output(coil_B_1_pin, step[2] == '1')
-		GPIO.output(coil_B_2_pin, step[3] == '1')
+        GPIO.output(coil_A_1_pin, step[0] == '1')
+        GPIO.output(coil_A_2_pin, step[1] == '1')
+        GPIO.output(coil_B_1_pin, step[2] == '1')
+        GPIO.output(coil_B_2_pin, step[3] == '1')
 def down(steps):
-		for i in range(steps):
-			for step in sequence:
-				set_step(step)
-				time.sleep(0.005)
-				
+        for i in range(steps):
+            for step in sequence:
+                set_step(step)
+                time.sleep(0.005)
+                
 if os.path.exists("/dev/ttyACM0") == True:
-	ACM = '/dev/ttyACM0'
+    ACM = '/dev/ttyACM0'
 if os.path.exists("/dev/ttyACM1") == True:
-	ACM = '/dev/ttyACM1'
+    ACM = '/dev/ttyACM1'
 if os.path.exists("/dev/ttyACM2") == True:
-	ACM = '/dev/ttyACM2'
+    ACM = '/dev/ttyACM2'
 if os.path.exists("/dev/ttyACM3") == True:
-	ACM = '/dev/ttyACM3'
+    ACM = '/dev/ttyACM3'
 
 
 ser = serial.Serial('/dev/ttyACM0' ,9600)
 while True:
-	button = GPIO.input(2)
-	read_serial = ser.readline()
-	
-	try:
-		if int(read_serial) > 0 and int(read_serial) < 30:
-			print "Brood"
-		
-		elif int(read_serial) > 30 and int(read_serial) < 60:
-			print "Sinaasappel"
-		
-		elif int(read_serial) > 60 and int(read_serial) < 90:
+    button = GPIO.input(2)
+    read_serial = ser.readline()
+    
+    try:
+        if int(read_serial) > 0 and int(read_serial) < 30:
+            print "Brood"
+        
+        elif int(read_serial) > 30 and int(read_serial) < 60:
+            print "Sinaasappel"
+        
+        elif int(read_serial) > 60 and int(read_serial) < 90:
 
-			print "Banaan"
+            print "Banaan"
 
-		else:
-			print read_serial
-	except:
-		pass
-	
+        else:
+            print read_serial
+    except:
+        pass
+    
 
 
-	if button == False:
-		a = int(read_serial)
-		set_step('0000')
-		delay = 5
-		steps = a
-		down(int(steps)) 
-		
-	print "Product = {}".format(a)
+    if button == False:
+        a = int(read_serial)
+        set_step('0000')
+        delay = 5
+        steps = a
+        down(int(steps)) 
+        
+    print "Product = {}".format(a)
 
-	
+    
 
-		
+        
