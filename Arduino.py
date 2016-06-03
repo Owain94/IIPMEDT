3ae344902df0c021ac58dc583096eeab3f9bf42a
Maarten Paauw <maartenpaauw@gmail.com>
2016-06-03 11:53:43 +0200

    Arduino klasse werkt, whoo hooo

--- Arduino.py
+++ Arduino.py
@@ -1,6 +1,7 @@
 from Constants import Constants
 import os.path
 import serial
+import collections
 
 
 class Arduino:
@@ -19,8 +20,36 @@ class Arduino:
 
         self.__serial = serial.Serial(self.__used_amc, 9600)
 
-    def read_serial(self) -> str:
-        return self.__serial.readline()
+    def read_serial(self) -> int:
+        try:
+            return int(self.__serial.readline())
+        except:
+            return -1
+
+    def get_serial(self) -> int:
+        counter = collections.Counter(self.get_serial_list())
+
+        highest = 0
+        d_key = None
+
+        for key in counter.keys():
+            if counter[key] > highest:
+                highest = counter[key]
+                d_key = key
+
+        return d_key
+
+    def get_serial_list(self) -> list:
+        ser_list = []
+
+        for i in range(20):
+            while True:
+                ser = self.read_serial()
+                if 1023 >= ser > -1:
+                    ser_list.append(ser)
+                    break
+
+        return ser_list
 
     @property
     def amc(self) -> str:
@@ -29,7 +58,8 @@ class Arduino:
 
 def main() -> None:
     arduino = Arduino()
-    print(arduino.read_serial())
+
+    print(arduino.get_serial())
 
 if __name__ == '__main__':
     main()
