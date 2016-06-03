cf4bf89dc51c487a177dc52fc510d29fc8d39c7d
Maarten Paauw <maartenpaauw@gmail.com>
2016-06-03 15:49:31 +0200

    Bell klasse aangemaakt

--- Bell.py
+++ Bell.py
@@ -1,4 +1,6 @@
+import RPi.GPIO as GPIO
 import time
+import threading
 
 
 class Bell:
@@ -6,5 +8,19 @@ class Bell:
         self.__bell_output_pin = output_pin
 
     def ring(self, seconds_to_ring: float) -> None:
-        pass
-        # todo write code.
+        GPIO.output(self.__bell_output_pin, True)
+        time.sleep(seconds_to_ring)
+        GPIO.output(self.__bell_output_pin, False)
+        time.sleep(seconds_to_ring)
+
+    def ring_in_thread(self, seconds_to_ring: float) -> None:
+        t = threading.Thread(target=self.ring, args=(seconds_to_ring,))
+        t.start()
+
+
+def main() -> None:
+    bell = Bell(2)  # input pin
+    bell.ring_in_thread(1.0)
+
+if __name__ == '__main__':
+    main()
