80852d884987ed245e548eab594367cc5d13c9cf
Owain van Brakel <owain.vanbrakel@gmail.com>
2016-06-03 20:21:17 +0200

    Comments (User.py)

--- User.py
+++ User.py
@@ -1,6 +1,18 @@
 class User:
+    """
+    Klasse om bij te houden welke producten de gebruiker allemaal
+    heeft toegevoegd
+    """
     def __init__(self):
+        """
+        code die wordt uitgevoerd bij het instantiëren van de klasse
+        """
         self.__user_products = []
 
-    def add_product(self, product: str):
+    def add_product(self, product: str) -> None:
+        """
+        Voeg een product toe aan de lijst met producten
+
+        :param product: Het toe te voegen product als string
+        """
         self.__user_products.append(product)
