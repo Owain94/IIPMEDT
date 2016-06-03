f4b4eafbb0cc2c379f07849fcfbfce0c54dedbb8
Maarten Paauw <maartenpaauw@gmail.com>
2016-06-03 14:18:47 +0200

    Begonnen met de bel klasse

---
+++ Bell.py
@@ -0,0 +1,10 @@
+import time
+
+
+class Bell:
+    def __init__(self, output_pin: int) -> None:
+        self.__bell_output_pin = output_pin
+
+    def ring(self, seconds_to_ring: float) -> None:
+        pass
+        # todo write code.
