c2150f9c6d05b7e45b8688025e0b21378ff92362
Maarten Paauw <maartenpaauw@gmail.com>
2016-06-03 15:17:48 +0200

    Methode aangemaakt voor een ledje te laten branden in een thread

--- Led.py
+++ Led.py
@@ -1,24 +1,31 @@
 import RPi.GPIO as GPIO
 import time
-
+import threading
 
 class Led:
     def __init__(self, output_pin: int) -> None:
         self.__led_output_pin = output_pin
-
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.__led_output_pin, GPIO.OUT)
 
     def blink(self, seconds: float):
-        GPIO.output(self.__led_output_pin, GPIO.HIGH)
+        GPIO.output(self.__led_output_pin, True)
         time.sleep(seconds)
-        GPIO.output(self.__led_output_pin, GPIO.LOW)
+        GPIO.output(self.__led_output_pin, False)
         time.sleep(seconds)
+
+    def blink_in_thread(self, seconds: float):
+        t = threading.Thread(target=self.blink, args=(seconds,))
+        t.start()
 
 
 def main() -> None:
-    led = Led(12)  # input pin
-    led.blink(2)
+    led = Led(2)  # input pin
+    led.blink_in_thread(1.0)
+
+    for i in range(20):
+        print('Main thread')
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     main()
