55d12223db1e767a48fcede5b24d66505af9955e
Maarten Paauw <maartenpaauw@gmail.com>
2016-06-04 16:45:42 +0200

    Alvast een klasse aangemaakt voor de displays

---
+++ Display.py
@@ -0,0 +1,16 @@
+class Display:
+
+    def __init__(self, output_pins: list) -> None:
+        pass
+
+
+def main() -> None:
+    """
+    Code om de klasse te testen, deze code wordt niet uitgevoerd als de
+    klasse in een ander bestand wordt geimporteerd!
+    """
+
+# Zorg ervoor dat de main functie niet wordt uitgevoerd als de klasse
+# wordt geimporteerd
+if __name__ == '__main__':
+    main()
