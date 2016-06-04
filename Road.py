2554551e9380ad9ee5630b298e130851e89a316a
Maarten Paauw <maartenpaauw@gmail.com>
2016-06-04 14:38:30 +0200

    2 methodes aangemaakt voor de motor

--- Road.py
+++ Road.py
@@ -14,6 +14,22 @@ class Road:
         self.__end_switch = Button(end_switch_input_pin)
         self.__motor = Motor(motor_input_pins)
 
+    def move_to_begin(self) -> bool:
+        while not self.__begin_switch.is_pressed():
+            self.__motor.down(1)
+
+        self.__motor.up(20)
+
+        return True
+
+    def move_to_end(self) -> bool:
+        while not self.__end_switch.is_pressed():
+            self.__motor.up(1)
+
+        self.motor.down(20)
+
+        return True
+
     @property
     def begin_switch(self) -> Button:
         return self.__begin_switch
