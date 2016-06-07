5473c5033e778a40b08be69f3705870e64b543df
Maarten Paauw <maartenpaauw@gmail.com>
2016-06-07 11:47:31 +0200

    Stap nummers

--- Main.py
+++ Main.py
@@ -47,24 +47,38 @@ def clear() -> None:
 
 try:
     while True:
+        #  STAP 2
+        #  De state is 'initial' en de start button is ingedrukt.
         if state.is_state('initial') and button_start.is_pressed():
             state.current_state = 'button_start_pressed'
 
+        #  STAP 1
+        #  De state is 'initial'.
         elif state.is_state('initial'):
             if not led_start_red.thread_is_alive():
                 led_start_red.blink_in_thread(1.0)
 
+        #  STAP 4
+        #  De state is 'button_start_pressed' en de telefoon knop
+        #  is niet ingedrukt.
         elif state.is_state('button_start_pressed')\
                 and not button_telephone.is_pressed():
             state.current_state = 'telephone_picked_up_for_first_time'
 
+        #  STAP 3
+        #  De state is 'button_start_pressed'
         elif state.is_state('button_start_pressed'):
             buzzer.buzz_in_thread()
 
+        #  STAP 5
+        #  De state is 'telephone_picked_up_for_first_time'
         elif state.is_state('telephone_picked_up_for_first_time'):
             state.current_state = 'telephone_first_track_played'
             telephone.play_track('welcome_track')  # todo audio file
 
+        #  STAP 6
+        #  De state is 'telephone_first_track_played' en de telefoon
+        #  knop is ingedrukt.
         elif state.is_state('telephone_first_track_played')\
                 and button_telephone.is_pressed():
             led_plus_red.blink_in_thread(1.0)
@@ -74,27 +88,35 @@ try:
                 pass
             if button_done.is_pressed():
                 state.current_state = 'products_selected'
-                
+
+        #  STAP 7
+        #  De state is 'products_selected'
         elif state.is_state('products_selected'):
             motor.up(user.convert_score_to_motor(user.calculate_final_score(
                 user.calculate_health_score(user.user_products),
                 user.calculate_calorie_score(user.user_products))))
             state.current_state = 'telephone_feedback_first_score'
-            
+
+        #  STAP 8
+        #  De state is 'telephone_feedpack_first_score'
         elif state.is_state('telephone_feedback_first_score'):
             state.current_state = 'first_feedback_track_played'
             telephone.play_track(user.determine_feedback_playback(
                 user.calculate_health_score(user.user_products),
                 user.calculate_calorie_score(user.user_products)))
-            
+
+        #  STAP todo stap nummer
+        #  Hier wordt de state 'initial' en de displays worden gereset.
         else:
             state.reset_state()
             clear()
 
+        #  De huidige status wordt geprint.
         print('Huidige status: ' + state.current_state)
 
         sleep(0.1)
 
+#  Alles wordt opgeruimd.
 except KeyboardInterrupt:
     GPIO.cleanup()
     clear()
