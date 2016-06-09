64ccf3698f236fc388c2db8ae7c5b6a8084f7e89
Maarten Paauw <maartenpaauw@gmail.com>
2016-06-09 11:47:58 +0200

    Stapnummers

--- Main.py
+++ Main.py
@@ -51,43 +51,65 @@ def clear() -> None:
 
 
 # print de huidige status
-def status() -> None:
+def status(step) -> None:
     #  De huidige status wordt geprint.
+    print('Stap: ' + str(step))
     print('Huidige status: ' + state.current_state)
 
 # Alle GPIO pinnen worden op false gezet
 GPIOFuckUp()
 
+# Beide displays uitzetten
+clear()
+
+# zet het stapnummer
+step = 0
+
 try:
     while True:
+        #  STAP 2
         #  De state is 'initial' en de start button is ingedrukt.
         if state.is_state('initial') and button_start.is_pressed():
+            #  Zet het stapnummer
+            step = 2
             #  Verander de state naar 'button_start_pressed'
             state.current_state = 'button_start_pressed'
 
+        #  STAP 1
         #  De state is 'initial'.
         elif state.is_state('initial'):
+            #  Zet het stapnummer
+            step = 1
             #  Als het ledje nog niet knippert.
             if not led_start_red.thread_is_alive():
                 #  laat het ledje knipperen voor 1 seconden.
                 led_start_red.blink_in_thread(0.5)
 
+        #  STAP 4
         #  De state is 'button_start_pressed' en de telefoon knop
         #  is niet ingedrukt.
         elif state.is_state('button_start_pressed')\
                 and not button_telephone.is_pressed():
+            #  Zet het stapnummer
+            step = 4
             #  Verander de state naar 'telephone_picked_up_for_first_time'
             state.current_state = 'telephone_picked_up_for_first_time'
 
+        #  STAP 3
         #  De state is 'button_start_pressed'
         elif state.is_state('button_start_pressed'):
+            #  Zet het stapnummer
+            step = 3
             #  Kijk of de buzzer thread bestaat
             if not buzzer.buzzer_is_alive():
                 #  Speel de buzzer af in een thread
                 buzzer.buzz_in_thread(0.5)
 
+        #  STAP 5
         #  De state is 'telephone_picked_up_for_first_time'
         elif state.is_state('telephone_picked_up_for_first_time'):
+            #  Zet het stapnummer
+            step = 5
             #  Verander de state naar 'telephone_first_track_played'
             state.current_state = 'telephone_first_track_played'
             #  Even 2 seconden wachten voordat de audio af gaat spelen.
@@ -95,10 +117,13 @@ try:
             #  Speel de track Ontvangsbericht.mp3 af.
             telephone.play_track('Ontvangsbericht')
 
+        #  STAP 6
         #  De state is 'telephone_first_track_played' en de telefoon
         #  knop is ingedrukt.
         elif state.is_state('telephone_first_track_played')\
                 and button_telephone.is_pressed():
+            #  Zet het stapnummer
+            step = 6
             #  Wanneer de klaar knop niet ingedrukt is
             while not button_done.is_pressed():
                 #  Als de plus knop niet ingedrukt is
@@ -112,26 +137,33 @@ try:
                 elif not led_plus_red.thread_is_alive():
                     led_plus_red.blink_in_thread(0.5)
                 #  Print de huidige status.
-                status()
+                status(step)
             #  Als de klaar knop ingedrukt wordt
             if button_done.is_pressed():
                 #  Verander de status naar 'products_selected'
                 state.current_state = 'products_selected'
 
+        #  STAP 8
         #  De state is 'button_start_pressed' en de telefoon knop
         #  is niet ingedrukt.
         elif state.is_state('products_selected')\
                 and not button_telephone.is_pressed():
+            #  Zet het stapnummer
+            step = 8
             #  Verander de state naar 'telephone_picked_up_for_first_time'
             state.current_state = 'give_score_to_the_user'
 
+        #  STAP 7
         #  De state is 'button_start_pressed'
         elif state.is_state('products_selected'):
+            #  Zet het stapnummer
+            step = 7
             #  Kijk of de buzzer thread bestaat
             if not buzzer.buzzer_is_alive():
                 #  Speel de buzzer af in een thread
                 buzzer.buzz_in_thread(0.5)
 
+        #  STAP 9
         #  De state is 'products_selected'
         elif state.is_state('give_score_to_the_user'):
             #  Wacht 2 seconden voordat de gebruiker de telefoon oppakt.
@@ -141,6 +173,8 @@ try:
                 telephone.prepare_track_list(user.user_products))
             #  Controleert of het de eerste keer is.
             if user.is_first_run():
+                #  Zet het stapnummer
+                step = 9
                 #  Laat de score van de gebruiker op het eerste schermpje zien
                 display_one.show_digit(user.calculate_final_score())
             else:
@@ -152,24 +186,33 @@ try:
             #  Verander de state naar 'ring_telephone_for_score'
             state.current_state = 'ring_telephone_for_score'
 
+        #  STAP 11
         #  De state is 'button_start_pressed' en de telefoon knop
         #  is niet ingedrukt.
         elif state.is_state('ring_telephone_for_score')\
                 and not button_telephone.is_pressed():
+            #  Zet het stapnummer
+            step = 11
             #  Verander de state naar 'telephone_feedback_first_score'
-            state.current_state = 'telephone_feedback_first_score'
+            state.current_state = 'telephone_feedback_score'
 
+        #  STAP 10
         #  De state is 'button_start_pressed'
         elif state.is_state('ring_telephone_for_score'):
+            #  Zet het stapnummer
+            step = 10
             #  Kijk of de buzzer thread bestaat
             if not buzzer.buzzer_is_alive():
                 #  Speel de buzzer af in een thread
                 buzzer.buzz_in_thread(0.5)
 
+        #  STAP 12
         #  De state is 'telephone_feedback_first_score'
-        elif state.is_state('telephone_feedback_first_score'):
-            #  Verander de state naar 'telephone_first_track_played'
-            state.current_state = 'telephone_first_track_played'
+        elif state.is_state('telephone_feedback_score'):
+            #  Zet het stapnummer
+            step = 12
+            #  Verander de state naar 'telephone_feedback_score'
+            state.current_state = 'telephone_feedback_score'
             #  Wacht 2 seconden voor het afspelen van de telefoon
             sleep(2.0)
             #  Speel de bijpassende feedback af op de telefoon.
@@ -192,7 +235,7 @@ try:
             # clear()
 
         #  Print de huidige status.
-        status()
+        status(step)
 
         #  De loop wordt 10 per seconden afgespeeld
         sleep(0.1)
